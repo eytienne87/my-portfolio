55bec6693ede0eb0e73ba22e5b26c19ba56b0224
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-10-07 11:18:14 -0400

    implemented the customer repository

---
+++ food-delivery-redo/app/repositories/customer_repository.rb
@@ -0,0 +1,50 @@
+require 'csv'
+require_relative '../models/customer'
+
+class CustomerRepository
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @customers = []
+    @next_id = 1
+    load_csv if File.exist?(@csv_file)
+  end
+
+  def all
+    @customers
+  end
+
+  def add_one_customer(customer)
+    customer.id = @next_id
+    @next_id += 1
+    @customers << customer
+    save_csv
+  end
+
+  def find_customer_by_id(id)
+    @customers.find do |customer|
+      customer.id == id
+    end
+  end
+
+  private
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+
+    CSV.foreach(@csv_file, csv_options) do |row|
+      row[:id] = row[:id].to_i
+
+      customer = customer.new(row)
+      @customers << customer
+    end
+    @next_id = @customers.empty? ? 1 : @customers.last.id + 1
+  end
+
+  def save_csv
+    CSV.open(@csv_file, 'wb') do |csv|
+      csv << ['id','name','address']
+      @customers.each { |customer| csv << [customer.id, customer.name, customer.address] }
+    end
+  end
+
+end
