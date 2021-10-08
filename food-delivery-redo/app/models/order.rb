e6e431ba55429875b52d80d5a653b30b1f84f70c
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-10-07 21:38:29 -0400

    implemented order model

---
+++ food-delivery-redo/app/models/order.rb
@@ -0,0 +1,9 @@
+class Order
+  def inititalize(attributes = {})
+    @id = attributes[:id]
+    @meal = attributes[:meal]
+    @customer = attributes[:customer]
+    @employee = attributes[:employee]
+    @delivered = attributes[:delivered] || false
+  end
+end
