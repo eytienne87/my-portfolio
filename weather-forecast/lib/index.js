069b7ab058ca548649b6958375557833cb0e1530
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-10-23 01:54:58 -0400

    added a capitalize function to capitalize the weather type, changed font size and weight of text and positioned elements in the main div

--- weather-forecast/lib/index.js
+++ weather-forecast/lib/index.js
@@ -1,7 +1,11 @@
 const apiKey = process.env.API_KEY
+
+const cityDiv = document.getElementById('location')
+const timeDiv = document.getElementById('timestamp')
 const weatherDiv = document.getElementById('weather-type')
 const celsiusDiv = document.getElementById('celsius-with-icon')
 
+// Function for converting a unix into a timestamp
 const timestampGenerator = (unix) => {
   const milliseconds = unix * 1000
   const dateObject = new Date(milliseconds)
@@ -12,20 +16,31 @@ const timestampGenerator = (unix) => {
      hour: "numeric"
     })
 };
-
+// Function to capitalize letter of each word
+const capitalize = (str) => {
+  const arr = str.split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  const str2 = arr.join(" ");
+  return str2
+};
 
 const extractData = (data) => {
+  const city = data.name
   const date = data.dt
   const timestamp = timestampGenerator(date)
-  const weatherType = data.weather[0].description
+  const weatherType = capitalize(data.weather[0].description)
   const celsius = Math.round(data.main.temp)
   const iconId = data.weather[0].icon
   celsiusDiv.insertAdjacentHTML('afterbegin',
-                                `<div class="container d-flex justify-content-center align-items-center">
+                                `<div class="container d-flex justify-content-between align-items-center p-0">
+                                  <p class="degree mb-0">${celsius}°C</p>
                                   <img src="http://openweathermap.org/img/wn/${iconId}@2x.png" alt="icon">
-                                  <p class="mb-0">${celsius}°C</p>
                                  </div>`)
   weatherDiv.insertAdjacentHTML('afterbegin', `<p class="">${weatherType}</p>`)
+  timeDiv.insertAdjacentHTML('afterbegin', `<p class="">${timestamp}</p>`)
+  cityDiv.insertAdjacentHTML('afterbegin', `<p class="">Weather in ${city}</p>`)
 };
 
 // fetch weather data from OpenWeatherMap API
@@ -38,4 +53,4 @@ const callApi = (city) => {
     );
 };
 
-callApi('montreal')
+callApi('los angeles')
